d6308beb507770c7c389b89fea26117ee5c58c53
lds <lvds8108@163.com>
2017-10-14 17:00:09 +0800

    add druid log

--- api-test/api-test-web/src/main/java/com/lyu/study/test/config/TestApiConfiguration.java
+++ api-test/api-test-web/src/main/java/com/lyu/study/test/config/TestApiConfiguration.java
@@ -7,16 +7,11 @@
 
 package com.lyu.study.test.config;
 
-import com.lyu.study.test.TestApiStarter;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import io.shardingjdbc.core.api.ShardingDataSourceFactory;
-import io.shardingjdbc.core.api.config.ShardingRuleConfiguration;
-import io.shardingjdbc.core.api.config.TableRuleConfiguration;
-import io.shardingjdbc.core.api.config.strategy.InlineShardingStrategyConfiguration;
-import io.shardingjdbc.core.yaml.sharding.YamlShardingDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -26,7 +21,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -35,44 +29,43 @@ import java.util.Map;
 @Configuration
 public class TestApiConfiguration {
 
-//    @Value("${sharding.config_file}")
-//    private String shardingConfigFile = "";
+    @Value("${sharding.config_file}")
+    private String shardingConfigFile = "";
 
-//    @Bean(name="ds_0")
-//    @Primary
-//    @ConfigurationProperties(prefix = "spring.datasource.ds_0")
-//    public DataSource primaryDataSource() {
-//        return DataSourceBuilder.create().build();
-//    }
-//
-//    @Bean(name = "ds_1")
-//    @ConfigurationProperties(prefix = "spring.datasource.ds_1")
-//    public DataSource secondDataSource() {
-//        return DataSourceBuilder.create().build();
-//    }
+    @Primary
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.one")
+    public DataSource dataSourceOne(){
+        return DruidDataSourceBuilder.create().build();
+    }
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.two")
+    public DataSource dataSourceTwo(){
+        return DruidDataSourceBuilder.create().build();
+    }
 
-//    @Bean(name = "sqlSessionFactory")
-//    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
-//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-//        bean.setDataSource(dataSource);
-//        return bean.getObject();
-//    }
-//
-//    @Bean
-//    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-//        return new SqlSessionTemplate(sqlSessionFactory);
-//    }
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(getShardingDataSource());
+        return bean.getObject();
+    }
 
-//    private DataSource getShardingDataSource() throws SQLException, IOException {
-//        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext();
-//        return ShardingDataSourceFactory.createDataSource(createDataSourceMap(),
-//                ctx.getResource(shardingConfigFile).getFile());
-//    }
-//
-//    private Map<String, DataSource> createDataSourceMap() {
-//        Map<String, DataSource> result = new HashMap<>(2, 1);
-//        result.put("ds_0", primaryDataSource());
-//        result.put("ds_1", secondDataSource());
-//        return result;
-//    }
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    private DataSource getShardingDataSource() throws SQLException, IOException {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext();
+        return ShardingDataSourceFactory.createDataSource(createDataSourceMap(),
+                ctx.getResource(shardingConfigFile).getFile());
+    }
+
+    private Map<String, DataSource> createDataSourceMap() {
+        Map<String, DataSource> result = new HashMap<>(2, 1);
+        result.put("ds_0", dataSourceOne());
+        result.put("ds_1", dataSourceTwo());
+        return result;
+    }
 }
